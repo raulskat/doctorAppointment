973ced415beae62c7dc5b4e6191c1b74c3c4e44b
raulskat <rahulshekhawat5992@gmail.com>
2025-06-19 17:26:32 +0530

    chore(appointments): change appointment_date to scheduled_on with timestamp type chore(doctors): remove email field from Doctor entity chore(migrations): add initial migration for hashedRefreshToken in user table chore(users): add hashedRefreshToken field to User entity

--- src/users/entities/user.entity.ts
+++ src/users/entities/user.entity.ts
@@ -28,6 +28,9 @@ export class User {
   @Column({ type: 'enum', enum: UserRole })
   role: UserRole;
 
+  @Column({ nullable: false })
+  hashedRefreshToken: string;
+
   @CreateDateColumn()
   created_at: Date;
 
