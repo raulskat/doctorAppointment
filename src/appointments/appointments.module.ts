f40cd8cd36641fa69a2e1b9edb552eb7aed6fa41
raulskat <rahulshekhawat5992@gmail.com>
2025-07-08 11:33:55 +0530

    feat(appointments): add DoctorAvailability entity to AppointmentsModule imports

--- src/appointments/appointments.module.ts
+++ src/appointments/appointments.module.ts
@@ -6,10 +6,12 @@ import { Appointment } from './entities/appointment.entity';
 import { DoctorTimeSlot } from 'src/availabilities/entities/doctor-timeslot.entity';
 import { Doctor } from 'src/doctors/entities/doctor.entity';
 import { AuthModule } from 'src/auth/auth.module';
+import { DoctorAvailability } from 'src/availabilities/entities/doctor-availability.entity';
 
 @Module({
   imports: [
-    TypeOrmModule.forFeature([Appointment, DoctorTimeSlot, Doctor]),AuthModule,
+    TypeOrmModule.forFeature([Appointment, DoctorTimeSlot, Doctor,
+      DoctorAvailability,]),AuthModule,
   ],
   
   providers: [AppointmentsService],
